b7572d7ffadcb196ef1ac065e30bebcb1fdf63b3
mhartington <mikehartington@gmail.com>
2016-03-31 12:08:24 -0400

    update

--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -1,11 +1,17 @@
-import {Page} from 'ionic-angular';
-
+import {Page, NavController} from 'ionic-angular';
+import {Page2} from '../page2/page2';
 
 @Page({
   templateUrl: 'build/pages/page1/page1.html',
 })
 export class Page1 {
-  constructor() {
+  constructor(
+    public nav: NavController
+  ) {
 
+  }
+
+  pushPage(){
+    this.nav.push(Page2)
   }
 }
